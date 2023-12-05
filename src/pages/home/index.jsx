f3886b77e0c7b57c6b8429a942c5471fa62ca107
Koutsumi <febaccarini@gmail.com>
2023-12-04 21:02:14 -0300

    changesFaq

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,7 +1,7 @@
 import MakeYourOwnArt from "../../components/makeYourOwnArt/makeYourOwnArt";
 import AboutUs from "../../components/aboutUs/aboutUs";
 import Coments from "../../components/coments/coments";
-import Contact from "../../components/contact/contact";
+import Faq from "../../components/faq/faq";
 import Address from "../../components/address/address";
 import IconWhatsapp from "../../images/iconWhatsapp";
 
@@ -11,7 +11,7 @@ function Home() {
       <MakeYourOwnArt />
       <AboutUs />
       <Coments />
-      <Contact />
+      <Faq />
       <Address/>
       <div className="fixed right-4 bottom-4">
       <a href="">
