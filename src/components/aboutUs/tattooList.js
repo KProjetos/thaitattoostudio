28851f3059a82a67e0f0e1d991fe91fb07825464
Fernanda <febaccarini@gmail.com>
2024-02-21 21:23:03 -0300

    feat remove carousel for desktop

--- src/components/aboutUs/tattooList.js
+++ src/components/aboutUs/tattooList.js
@@ -22,10 +22,10 @@ export default{
                 name:"Realismo",
                 image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/realismoWEB.webp?alt=media&token=fda62345-9b38-4e2b-a975-77f164f7ef09"
             },
-            {
-                name:"Tattoo combinando",
-                image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/tattooAmigaWEB.webp?alt=media&token=94791c63-7cde-4108-a2e8-4e9f7a9ac3a2"
-            },
+            // {
+            //     name:"Tattoo combinando",
+            //     image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/tattooAmigaWEB.webp?alt=media&token=94791c63-7cde-4108-a2e8-4e9f7a9ac3a2"
+            // },
             {
                 name:"Blackwork",
                 image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/blackwork2WEB.webp?alt=media&token=31d4a3db-2d5b-4373-b1cc-b9bc4f556c0f"
@@ -34,46 +34,50 @@ export default{
                 name: "Traços finos",
                 image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/leoaWEB.webp?alt=media&token=a0566761-bedf-4ec0-9947-7cc7b4223096"
             },
-            {
-                name: "Traços finos",
-                image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/floralWEB.webp?alt=media&token=a4ccfdca-13c1-477c-80c0-2c1ed2b17276"
-            },
+            // {
+            //     name: "Traços finos",
+            //     image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/floralWEB.webp?alt=media&token=a4ccfdca-13c1-477c-80c0-2c1ed2b17276"
+            // },
             {
                 name:"Geek",
                 image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/geekWEB.webp?alt=media&token=5acfd66c-f94f-401c-a2b5-c32f1ec23aff"
             },
+            // {
+            //     name:"Maori",
+            //     image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/maoriWEB.webp?alt=media&token=fe775300-8fe5-4435-8c6e-013780d30901"
+            // },
+            // {
+            //     name:"Patch",
+            //     image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/patchWEB.webp?alt=media&token=53421d80-774e-4e6f-a08d-d013f97cffde"
+            // },
+            // {
+            //     name:"Aquarela",
+            //     image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/aquarelaRedWEB.webp?alt=media&token=8837655b-ee10-469b-abe6-65d627c25a38"
+            // },
+            // {
+            //     name: "Realismo",
+            //     image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/realismoPanteraWEB.webp?alt=media&token=aa5cc711-8a16-427f-ac84-cac4844419ff"
+            // },
+            // {
+            //     name:"Sketch",
+            //     image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/sketchWEB.webp?alt=media&token=20d1ee07-29ff-4245-9489-993a59f570f2"
+            // },
+            // {
+            //     name:"Pontilhismo",
+            //     image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/pontialhismoWEB.webp?alt=media&token=9a076012-64a6-4733-ae9c-3638eeebcfaa"
+            // },
+            // {
+            //     name:"Revitalização",
+            //     image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/revitalizacaoWEB.webp?alt=media&token=c035dc2b-0409-43f9-92ed-90415b320eb2"
+            // },
+            // {
+            //     name:"Cobertura",
+            //     image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/coberturaWEB.webp?alt=media&token=24b1ea2c-dcf2-48cb-9521-c42556091bbe"
+            // },
             {
-                name:"Maori",
-                image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/maoriWEB.webp?alt=media&token=fe775300-8fe5-4435-8c6e-013780d30901"
-            },
-            {
-                name:"Patch",
-                image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/patchWEB.webp?alt=media&token=53421d80-774e-4e6f-a08d-d013f97cffde"
-            },
-            {
-                name:"Aquarela",
-                image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/aquarelaRedWEB.webp?alt=media&token=8837655b-ee10-469b-abe6-65d627c25a38"
-            },
-            {
-                name: "Realismo",
-                image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/realismoPanteraWEB.webp?alt=media&token=aa5cc711-8a16-427f-ac84-cac4844419ff"
-            },
-            {
-                name:"Sketch",
-                image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/sketchWEB.webp?alt=media&token=20d1ee07-29ff-4245-9489-993a59f570f2"
-            },
-            {
-                name:"Pontilhismo",
-                image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/pontialhismoWEB.webp?alt=media&token=9a076012-64a6-4733-ae9c-3638eeebcfaa"
-            },
-            {
-                name:"Revitalização",
-                image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/revitalizacaoWEB.webp?alt=media&token=c035dc2b-0409-43f9-92ed-90415b320eb2"
-            },
-            {
-                name:"Cobertura",
-                image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/coberturaWEB.webp?alt=media&token=24b1ea2c-dcf2-48cb-9521-c42556091bbe"
-            },
+                name: "Cobertura/Rerforma",
+                image: "https://firebasestorage.googleapis.com/v0/b/thai-tattoo-studio-42df4.appspot.com/o/reformaTattoo.jpg?alt=media&token=58624b9c-d34e-4e01-a0ca-8ba1c4491e02"
+            }
         ]
         return data_tattos;
     }
