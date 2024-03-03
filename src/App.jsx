05805b9f371944122b0598885d1b6d39f5948378
Fernanda <febaccarini@gmail.com>
2024-03-03 11:46:32 -0300

    feat add modal for accept cookies

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,25 @@ import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
 
 function App() {
+
+  window.onload = function () {
+    var consentCookie =
+      document.cookie.indexOf("cookieConsent=accepted") !== -1;
+    var cookieBanner = document.getElementById("cookieBanner");
+
+    if (!consentCookie && cookieBanner) {
+      cookieBanner.style.display = "block";
+    }
+  };
+
+  function acceptCookies() {
+    document.cookie =
+      "cookieConsent=accepted; expires=Fri, 31 Dec 9999 23:59:59 GMT; path=/";
+    var cookieBanner = document.getElementById("cookieBanner");
+    if (cookieBanner) {
+      cookieBanner.style.display = "none";
+    }
+  }
   
 
   return (
@@ -12,7 +31,13 @@ function App() {
     <RoutesApp/>
 
     <Footer/>
-
+    
+    <div id="cookieBanner">
+      Este site usa cookies para garantir que você obtenha a melhor experiência. 
+      <button type="submit" onClick={acceptCookies}>
+        Aceitar
+      </button>
+    </div>
     </>
   )
 }
